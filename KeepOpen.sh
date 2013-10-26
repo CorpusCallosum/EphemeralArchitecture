58f111b6dae5f1d2beb9014400587c09824fc5f7
auvibrant <auvproj@gmail.com>
2013-10-26 14:34:38 -0400

    minor update to keepopen.sh cleanup

--- KeepOpen.sh
+++ KeepOpen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $(ps ax | grep -v grep | grep "LANscapes.app" | wc -l) -eq 0 ]
 then
-       echo "Twitter not running. opening..."
+       echo "LANscalpes not running. opening..."
        open /Applications/LANscapes.app
 else
     echo "LANscapes running"
