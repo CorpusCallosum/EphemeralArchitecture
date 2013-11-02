ac0a7ac2400f34f35f707a10a0eb054f0470dd8f
Jack Kalish <jack@jackkalish.com>
2013-11-02 15:34:08 -0400

    addinf new ofx port

---
+++ lanscapes_ofx/build/LANscapes.build/Debug/LANscapes.build/Objects-normal/i386/usb_libusb10.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Volumes/HD2/OpenFrameworks/of_v0.8.0_osx_release/addons/ofxKinect/libs/libfreenect/src/usb_libusb10.c \
+  /Volumes/HD2/OpenFrameworks/of_v0.8.0_osx_release/apps/myApps/LANscapes/../../../addons/ofxKinect/libs/libusb-1.0/include/libusb-1.0/libusb.h \
+  ../../../addons/ofxKinect/libs/libfreenect/src/freenect_internal.h \
+  /Volumes/HD2/OpenFrameworks/of_v0.8.0_osx_release/apps/myApps/LANscapes/../../../addons/ofxKinect/libs/libfreenect/include/libfreenect.h \
+  /Volumes/HD2/OpenFrameworks/of_v0.8.0_osx_release/apps/myApps/LANscapes/../../../addons/ofxKinect/libs/libfreenect/include/libfreenect-registration.h \
+  ../../../addons/ofxKinect/libs/libfreenect/src/usb_libusb10.h \
+  ../../../addons/ofxKinect/libs/libfreenect/src/loader.h
