9918f4fb37822aa0a54d0fa0eb0244dde8f3ed8a
gabriella levine <gabriella.levine@gmail.com>
2013-11-25 18:49:26 -0500

    tiny change to booleans in gui class

--- lanscapesC_kinect/src/lanscapes.cpp
+++ lanscapesC_kinect/src/lanscapes.cpp
@@ -21,8 +21,8 @@ void lanscapes::setup(){
     //setup vars default values
     //PRESS B TO CAPTURE BACKGROUND//
     fullscreen = false; // f 
-    bDrawVideo = false;  // v
-    bWireframe = true;  // w draw wireframe mesh
+    bDrawVideo = gui.drawVideo();  // v
+    bWireframe = gui.isWireOn();  // w draw wireframe mesh
     bFaces = true;      // e draw faces of main mesh
     //Set this to FALSE to use webcam
     useKinect = false;
