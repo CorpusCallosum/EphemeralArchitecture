d673a25fa0392948b9c7dc76b05cba80f88e5346
Jack Kalish <jykalish@gmail.com>
2014-07-15 16:35:50 -0700

    fix type for sh file

--- KeepOpen.sh
+++ KeepOpen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $(ps ax | grep -v grep | grep "LANscapesDebug.app" | wc -l) -eq 0 ]
 then
-echo "landscapes not running. opening…"
+echo "LANscapes not running. opening…"
 open /OpenFrameworks/of_v0.8.0_osx_release/apps/LANscapes/lanscapes_sound/bin/LANscapesDebug.app
 else
 echo "LANscapes running"
