4b39a936ba38b38535f24c035dcb599253741b73
Jack Kalish <jack@jackkalish.com>
2014-07-16 12:31:43 -0700

    putting sh files back in,,,

---
+++ KeepOpen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ $(ps ax | grep -v grep | grep "LANscapesDebug.app" | wc -l) -eq 0 ]
+then
+echo "LANscapes not running. opening…"
+open /OpenFrameworks/of_v0.8.0_osx_release/apps/LANscapes/lanscapes_sound/bin/LANscapesDebug.app
+else
+echo "LANscapes running"
+fi
